49f12d7654f05dbf9246b8168ce0ca1e156c64de
Ayako Woollan <ayako.woollan@gmail.com>
2022-03-02 16:00:49 +1100

    updating api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,8 @@
-// connecting two routes
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const blogRoutes = require('./blogRoutes');
+
+router.use('/users', userRoutes);
+router.use('/blogs', blogRoutes);
+
+module.exports = router;
