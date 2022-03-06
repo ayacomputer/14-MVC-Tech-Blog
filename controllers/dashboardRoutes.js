2a046c162f4ff99b4b469a995105b0af25966fa0
Ayako Woollan <ayako.woollan@gmail.com>
2022-03-06 16:11:36 +1100

    updating dashboardRoutes/server.js/views

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -5,6 +5,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
+        console.log('working')
 
     } catch (err) {
         res.status(400).json(err);
@@ -14,8 +15,11 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-
+        console.log('working')
     } catch (err) {
         res.status(400).json(err);
     }
 });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helper = require('./utils/helper');
+const helpers = require('./utils/helper');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helper });
+const hbs = exphbs.create({ helpers });
 
 
 const sess = {
